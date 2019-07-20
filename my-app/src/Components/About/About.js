c5c3155da90922a42715e20a73bc29853fd0c75a
ido <idoishere2@gmail.com>
2019-07-20 15:47:32 +0300

    fixed

--- my-app/src/Components/About/About.js
+++ my-app/src/Components/About/About.js
@@ -4,7 +4,7 @@ import resume from '../../images/resume.pdf'
 import AboutLogo from './AboutLogo/AboutLogo'
 import './About.css'
 
-function About({ resumeData,sortProjects }) {
+function About({ resumeData, sortProjects }) {
   const parallaxData = [
     {
       start: 0,
@@ -36,20 +36,23 @@ function About({ resumeData,sortProjects }) {
         </div>
         <div className="contact-details">
           <h2 className="contactTitle">Contact Details</h2>
-          <p>
-            <span>{resumeData.name}</span><br />
-            <span>{resumeData.home}<br />
-              {resumeData.address}
-            </span><br />
-            <span>{resumeData.phone}</span><br />
-            <span>{resumeData.email}</span>
-          </p>
-          <div className="resumeDiv">    
-       <a className = "Resume" href={resume}>Resume</a>           
+          <div className="my-data">
+
+            <p>     {resumeData.name} </p>
+            <p>    {resumeData.address}</p>
+            <p>
+
+
+              {resumeData.email}
+            </p>
+          </div>
+
+          <div className="resumeDiv">
+            <a className="Resume" href={resume}>Resume</a>
           </div>
         </div>
       </div>
-   {/* <AboutLogo sortProjects={sortProjects}/>      */}
+      {/* <AboutLogo sortProjects={sortProjects}/>      */}
       <div>
       </div>
     </div>
